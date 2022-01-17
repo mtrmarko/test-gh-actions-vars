b8a122255f62e983be0899b59e42b1555ad96a74
mtrmarko <mtrmarko@users.noreply.github.com>
2022-01-17 13:50:58 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,3 @@
-# alias python3 as python
-#sudo apt install python-is-python3
-
-# install python2
-#sudo apt install python2
-
 # Install QEMU Guess Agent
 sudo apt install qemu-guest-agent
 
@@ -16,26 +10,24 @@ curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # install packages for native node package development
-sudo apt-get install gcc g++ make
+sudo apt-get install -y gcc g++ make
 
 # run this to install pm2. It looks like it may install serve too so ignore next npm install serve line
-npm install -g pm2
-# probably do not need to run this
-#npm install -g serve
+sudo npm install -g pm2
 
 # install yarn
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update && sudo apt-get install -y yarn
 
 # install git
-sudo apt install git
+sudo apt install -y git
 
 # install misc packages
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
 # These are needed for Cypress integration testing
-sudo apt install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+sudo apt install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 
 
 # install docker
@@ -43,44 +35,19 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # install docker compose (replace 2.2.2 with desired version)
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# create ~/projects directory and clone repos
-mkdir ~/projects
-cd ~/projects/
-git clone https://github.com/NuclearnAI/frontend.git
-git clone https://github.com/NuclearnAI/model-runtime-engine.git
-
-# update .env file for model-runtime-engine
-cd model-runtime-engine/
-nano .env
-
 # install python 3.8 virtual environments
-sudo apt install python3.8-venv
-sudo apt install python3-pip
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9 python3.9-venv -y
 
-# Need to be inside the MRE project/repo folder first
-cd mre # go to mre subdir of repo before creating venv
-python -m venv venv
-. venv/bin/activate
-# DO NOT run as root
-pip install pip-tools
-cd .. # go back to root of repo
-pip-sync requirements.txt dev-requirements.txt
-cd mre
-make run
-
-
-## Update setup.py to reuire python 3.8. Brad has it st 3.9
-
-# start model runtime engine
-make devenv
-
-# install react-scripts and start frontend
-cd ../frontend
-yarn add react-scripts
-yarn start
+# Enable Git credential store and store and set default user info
+git config --global credential.helper store
+git config --global user.email "marko@nuclearn.ai"
+git config --global user.name "Marko Mitrovic"
